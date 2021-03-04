b33b87a7d61cbc951a242d9a9681e3ae08fc0f59
Яна <ianagez88@gmail.com>
2021-03-04 15:19:52 +0200

    task1 package added

---
+++ src/main/java/com/tasks/task1/ChessBoard.java
@@ -0,0 +1,28 @@
+package com.tasks.task1;
+
+public class ChessBoard {
+    private int height;
+    private int width;
+
+    public ChessBoard(int height, int width){
+        this.height=height;
+        this.width=width;
+    }
+
+    public void draw(){
+        char even='*';
+        char odd=' ';
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if(j%2==0){
+                    System.out.print(even);
+                }else
+                    System.out.print(odd);
+            }
+            System.out.println();
+            char temp=even;
+            even=odd;
+            odd=temp;
+        }
+    }
+}
