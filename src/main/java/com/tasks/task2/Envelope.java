f921609dc27cd76e9a8605db17beb35f53d99821
Яна <ianagez88@gmail.com>
2021-03-09 09:27:43 +0200

    RectangleManipulator class is added

--- src/main/java/com/tasks/task2/Envelope.java
+++ src/main/java/com/tasks/task2/Envelope.java
@@ -1,32 +1,8 @@
 package com.tasks.task2;
 
-public class Envelope {
+public class Envelope extends Rectangle{
 
-    private double height;
-    private double width;
-
-    private double longSide;
-    private double shortSide;
-    private double area;
-
-    public Envelope(double height,double width){
-        this.height=height;
-        this.width=width;
-        area=height*width;
-        longSide=Math.max(height,width);
-        shortSide=Math.min(height,width);
-    }
-
-    public boolean putInto(Envelope other){
-        if(Math.abs(this.area-other.area) <0.001)
-            return false;
-
-        Envelope smaller=this.area<other.area ? this : other;
-        Envelope bigger=this.area>other.area ? this : other;
-
-        if(smaller.shortSide<bigger.shortSide && smaller.longSide<bigger.longSide)
-            return true;
-        return Math.sqrt((Math.pow(bigger.shortSide,2) + Math.pow(bigger.shortSide,2)))>
-                (smaller.shortSide+smaller.longSide);
+    public Envelope(double height, double width) {
+        super(height, width);
     }
 }
