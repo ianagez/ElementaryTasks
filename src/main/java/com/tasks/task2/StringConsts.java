fb00cd20bad82c7791d039a436a91f0c348cac48
Яна <ianagez88@gmail.com>
2021-03-10 21:22:19 +0200

    Class Task2 class refactored

---
+++ src/main/java/com/tasks/task2/StringConsts.java
@@ -0,0 +1,17 @@
+package com.tasks.task2;
+
+public interface StringConsts {
+    String FIRST_MESSAGE="Let's check if envelopes fit one into another.";
+    String ENTER_SIDE="Enter side";
+    String IS_FITTED="Yes! It is possible to fit one envelope into another.";
+    String IS_NOT_FITTED="No! It is impossible to fit one envelope into another.";
+    String LIKE_TO_CONTINUE="Would you like to continue?";
+    String WRONG_NUMBER_FORMAT="Wrong number format.";
+    String MORE_THAN_ZERO="Height and width should be more than 0.";
+    String Y="y";
+    String YES="yes";
+    String BYE="Bye-bye!";
+
+    String[] SIDE_NAMES= {"a","b","c","d"};
+}
+
--- src/main/java/com/tasks/task2/Task.java
+++ src/main/java/com/tasks/task2/Task.java
@@ -1,15 +1,11 @@
 package com.tasks.task2;
 
 public abstract class Task {
-    private Messenger messenger;
-    private String[] args;
+    protected Messenger messenger;
 
     public Task(Messenger messenger){
         this.messenger=messenger;
     }
-    public Task(Messenger messenger, String[] args){
-        this.messenger=messenger;
-        this.args=args;
-    }
-    abstract protected void run();
+
+    abstract public void run();
 }
